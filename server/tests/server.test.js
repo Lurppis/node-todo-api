bc691bcf3e62582c8ae733514e2edf326fd5d219
Marcin Pilarczyk <marcinpilarczyk@MacBook-Air-Marcin.local>
2018-01-29 17:57:36 +0100

    Prepare test for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,12 +4,16 @@ const request = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+const dumyTodos = [{ text: 'First test todo' }, { text: 'Second test todo' }, { text: 'Third test todo' }];
+
 // Will delete all documents to make sure that test will execute correctly
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(dumyTodos);
+	}).then(() => done());
 });
 
-describe('Positive test server', () => {
+describe('POST /todos', () => {
 	it('Should create new todo', (done) => {
 		var text = 'Test todo text2';
 
@@ -26,17 +30,13 @@ describe('Positive test server', () => {
 				if (err) {
 					return done(err);
 				}
-				Todo.find().then((todos) => {
+				Todo.find({ text }).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
 				}).catch((e) => done(e));
 			});
 	});
-
-});
-
-describe('Negative test server', () => {
 
 	it('Should not create new todo with bad data', (done) => {
 		var emptyText = '';
@@ -54,5 +54,19 @@ describe('Negative test server', () => {
 					done();
 				}).catch((e) => done(e));
 			});
+	});
+
+});
+
+describe('GET /todos', () => {
+
+	it('Should return all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(dumyTodos.length);
+			})
+			.end(done);
 	});
 });
