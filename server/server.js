1a94033ea1ae58cd5e9352c36e46efa068a6e4d3
Marcin Pilarczyk <marcinpilarczyk@MacBook-Air-Marcin.local>
2018-01-30 13:55:38 +0100

    Added patch route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -48,7 +49,6 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
 	var id = req.params.id;
-	console.log(id);
 	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
@@ -57,10 +57,35 @@ app.delete('/todos/:id', (req, res) => {
 		if (!todo) {
 			return res.status(404).send();
 		}
-		res.send({todo});
+		res.send({ todo });
 	}, (err) => {
 		res.status(400).send(err);
 	}).catch((e) => res.send(e));
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then((todo) => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.send({ todo });
+		}).catch((e) => {
+			res.status(404).send(e);
+		});
 });
 
 var server = app.listen(port, () => {
